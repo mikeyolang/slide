0f60e144d4b06aa81097bb20fe9cf54bb27bd97f
mikeyolang <brunomike965@gmail.com>
2023-10-30 13:25:18 +0300

    Finished the sign up page

--- lib/UI/screens/get_started.dart
+++ lib/UI/screens/get_started.dart
@@ -8,10 +8,370 @@ class GetStartedScreen extends StatefulWidget {
 }
 
 class _GetStartedScreenState extends State<GetStartedScreen> {
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _usernameController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+  final _institution = TextEditingController();
+  // The Form key
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    _emailController.dispose();
+    _passwordController.dispose();
+    _usernameController.dispose();
+    _confirmPasswordController.dispose();
+    _institution.dispose();
+    super.dispose();
+  }
+
+  String? _checkPassword(value) {
+    if (value.isEmpty) {
+      return "Please enter your password";
+    }
+    // Check if the password contains at least 8 characters.
+    if (value.length < 8) {
+      return "Password should contain at least 8 characters";
+    }
+    // Check if the password contains at least 1 uppercase letter.
+    if (!value.contains(RegExp(r'[A-Z]'))) {
+      return "Password should contain at least 1 uppercase letter";
+    }
+    // Check if the password contains at least 1 lowercase letter.
+    if (!value.contains(RegExp(r'[a-z]'))) {
+      return "Password should contain at least 1 lowercase letter";
+    }
+    // Check if the password contains at least 1 number.
+    if (!value.contains(RegExp(r'[0-9]'))) {
+      return "Password should contain at least 1 number";
+    }
+    // Check if the password contains at least 1 special character.
+    if (!value.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'))) {
+      return "Password should contain at least 1 special character";
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Let's Get Started",
+                style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.1,
+              ),
+              const Text(
+                "Sign Up to Hop in to this Slide Connect application",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w100,
+                  color: Colors.grey,
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.1,
+              ),
+              // Creating the Form for the User to fill
+              SingleChildScrollView(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Form(
+                        key: _formKey,
+                        child: Column(
+                          children: [
+                            const Text(
+                              "Enter Full Name",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w100,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            TextFormField(
+                              keyboardType: TextInputType.name,
+                              controller: _usernameController,
+                              decoration: const InputDecoration(
+                                hintText: "Your full name",
+                                hintStyle: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w100,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter your full name';
+                                } else if (value.length < 3) {
+                                  return 'Please enter a valid name';
+                                }
+                                return null;
+                              },
+                            ),
+                            // Field inputs for email
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            const Text(
+                              "Enter Email",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w100,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            TextFormField(
+                              keyboardType: TextInputType.emailAddress,
+                              controller: _emailController,
+                              decoration: const InputDecoration(
+                                hintText: "Your email",
+                                hintStyle: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w100,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter your email';
+                                } else if (!value.contains('@')) {
+                                  return 'Please enter a valid email';
+                                }
+                                return null;
+                              },
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            const Text(
+                              "Enter Institution",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w100,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            TextFormField(
+                              keyboardType: TextInputType.name,
+                              controller: _institution,
+                              decoration: const InputDecoration(
+                                hintText: "Your institution",
+                                hintStyle: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w100,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter your institution';
+                                } else if (value.length < 3) {
+                                  return 'Please enter a valid institution';
+                                }
+                                return null;
+                              },
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            const Text(
+                              "Enter Password",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w100,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            TextFormField(
+                              keyboardType: TextInputType.visiblePassword,
+                              controller: _passwordController,
+                              decoration: const InputDecoration(
+                                hintText: "Your password",
+                                hintStyle: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w100,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              //  Password should contain at least 8 characters, 1 uppercase, 1 lowercase, 1 number and 1 special character
+                              validator: (value) {
+                                return _checkPassword(value);
+                              },
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            const Text(
+                              "Confirm Password",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w100,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            TextFormField(
+                              keyboardType: TextInputType.visiblePassword,
+                              controller: _confirmPasswordController,
+                              decoration: const InputDecoration(
+                                hintText: "Confirm your password",
+                                hintStyle: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w100,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              //  Password should contain at least 8 characters, 1 uppercase, 1 lowercase, 1 number and 1 special character
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please confirm your password';
+                                } else if (value != _passwordController.text) {
+                                  return 'Passwords do not match';
+                                }
+                                return null;
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      // Terms and Conditions
+                      Row(
+                        children: [
+                          Checkbox(
+                            value: false,
+                            onChanged: (value) {
+                              setState(() {
+                                value = !value!;
+                              });
+                            },
+                          ),
+                          const Text(
+                            "I agree to the terms and conditions ",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w100,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              "Terms and Conditions",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blue,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      // Sign Up Button
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.blue,
+                        ),
+                        child: TextButton(
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  duration: Duration(seconds: 5),
+                                  content: Text('Processing Data'),
+                                ),
+                              );
+                            }
+                          },
+                          child: const Center(
+                            child: Text(
+                              "Get Started",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      // Already have an account
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        children: [
+                          const Text(
+                            "Already have an account?",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue,
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              "Sign In",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blue,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
